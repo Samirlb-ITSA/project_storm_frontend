c4f3d124f4ee93d70e4ea987eefd419c011ce2ab
Samir-LB <samirlorabastidas@gmail.com>
2023-11-13 22:36:53 -0500

    test

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,72 +1,23 @@
 import { useCallback, useState } from 'react'
 import {useDropzone} from 'react-dropzone'
 import bgimage from '../images/icon/icon-arrow-down.svg'
-import * as XLSX from 'xlsx';
-import { apiClient } from '../js/apiClient'; // adjust the path to your apiClient function
 
 interface UploadUsersProps {
-  file: string | ArrayBuffer | null,
-  setFile: React.Dispatch<React.SetStateAction<string | ArrayBuffer | null>>,
+  file: File | null,
+  setFile: React.Dispatch<React.SetStateAction<File | null>>,
   isPending: boolean,
   setIsPending: React.Dispatch<React.SetStateAction<boolean>>,
   url: string | null,
   setUrl: React.Dispatch<React.SetStateAction<string | null>>,
   setError: React.Dispatch<React.SetStateAction<boolean>>
+  summary: { users_created: number, users_failed: number } | null,
+  setSummary: React.Dispatch<React.SetStateAction<{ users_created: number, users_failed: number } | null>>
 }
 
-const UploadUsers: React.FC<UploadUsersProps> = ({file, setFile, isPending, setIsPending, url, setUrl, setError}) => {
-    const [successCount, setSuccessCount] = useState(0);
-    const [failureCount, setFailureCount] = useState(0);
-    const createUser = async(user: any) => {
-       setError(false)
-       setIsPending(true)
-       try{
-           const res = await apiClient('create_user', {
-             method: 'POST',
-             body: user,
-           });
-           if(!res.ok){
-            throw Error('Internal Server Error')
-           }
-           setSuccessCount(prevCount => prevCount + 1);
-           setIsPending(false)
-       }catch(error){
-           console.log(error)
-           setFailureCount(prevCount => prevCount + 1);
-           setIsPending(false)
-           setError(true)
-       }
-    }
-
-    const onDrop = useCallback(async(acceptedFiles: File[]) => {
-       let file = acceptedFiles[0]
-       let reader = new FileReader()
-
-       reader.onload = (e) => {
-        if(e.target !== null && e.target.result instanceof ArrayBuffer) {
-          const data = new Uint8Array(e.target.result);
-          const workbook = XLSX.read(data, {type: 'array'});
-          const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-          const jsonData = XLSX.utils.sheet_to_json(worksheet, {header: 1}) as any[][];
-          jsonData.forEach((row: any[], index: number) => {
-            if(index !== 0) { // Skip header row
-              const user = {
-                firstname: row[0],
-                lastname: row[1],
-                email: row[2],
-                cellphone: row[3],
-                address: row[4],
-                password: row[5],
-                status: 1,
-                roleId: 1,
-              };
-              createUser(user);
-            }
-          });
-        }
-       }
-
-       reader.readAsArrayBuffer(file);
+const UploadUsers: React.FC<UploadUsersProps> = ({file, setFile, isPending, setIsPending, url, setUrl, setError, summary, setSummary}) => {
+  const onDrop = useCallback(async(acceptedFiles: File[]) => {
+       let file = acceptedFiles[0];
+       setFile(file);
     } , [setFile])
 
     const {getRootProps , getInputProps , open} = useDropzone({onDrop , 
@@ -79,6 +30,30 @@ const UploadUsers: React.FC<UploadUsersProps> = ({file, setFile, isPending, setI
         noClick : true ,
         noKeyboard : true})
 
+        async () => {
+          setIsPending(true);
+          const formData = new FormData();
+          if (file) {
+              formData.append('file', file);
+              try {
+                  const response = await fetch('http://localhost:8000/upload', {
+                      method: 'POST',
+                      body: formData
+                  });
+                  const data = await response.json();
+                  setUrl(data.url); // assuming the response contains a url property
+                  setSummary({
+                      users_created: data.users_created, // replace with actual property names from your response
+                      users_failed: data.users_failed // replace with actual property names from your response
+                  });
+                  setIsPending(false);
+              } catch (error) {
+                  setError(true);
+                  setIsPending(false);
+              }
+          }
+          setIsPending(false);
+      };
   return (
     <div className='flex flex-col h-[50vh] drop-shadow-lg p-5 justify-between bg-white w-4/5 md:w-2/6 sm:w-4/6 rounded-md'>
       <p className='text-center font-semibold text-lg md:text-xl mb-2'>Sube tus usuarios desde un archivo de Excel</p>
@@ -89,9 +64,7 @@ const UploadUsers: React.FC<UploadUsersProps> = ({file, setFile, isPending, setI
          <p className='text-slate-400 md:text-md text-center mt-4 text-sm'>Arrastra & Suelta tu archivo aqui</p>
       </div>
       <p className='text-center font-normal text-slate-400 text-md mt-2 mb-2'>O</p>
-      <button onClick={open} className='bg-blue text-black font-normal p-1 rounded-lg w-auto mx-auto px-4 py-2 text-md'>Seleccina tu archivo</button>
-      <p>Users created successfully: {successCount}</p>
-      <p>Failed to create users: {failureCount}</p>
+      <button onClick={open} className='bg-blue text-black font-normal p-1 rounded-lg w-auto mx-auto px-4 py-2 text-md'>Upload File</button>
     </div>
   )
 }
--- src/pages/Dashboard/UploadUsers.tsx
+++ src/pages/Dashboard/UploadUsers.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
-import Form from '../../components/UploadForm'
+import UploadUsersProps from '../../components/UploadForm'
 import Pending from '../../components/PendingUpload'
 import Uploaded from '../../components/Uploaded';
 
 function UploadUsers() {
   const [isPending , setIsPending] = useState(false)
-  const [file , setFile] = useState<string | ArrayBuffer | null>(null)
+  const [file , setFile] = useState<File | null>(null)
+  const [summary, setSummary] = useState<{ users_created: number, users_failed: number } | null>(null);
   const [url , setUrl] = useState<string | null>(null)
   const [error , setError] = useState(false)
+
   return (
     <div className="w-full h-screen bg-grey flex justify-center items-center">
         {error ? <p className='text-red-600 text-center border-red-600 rounded-lg border-2 bg-red-300 px-4 py-2'>internal server error , Refresh the page and try again</p> :
-        isPending ? <Pending/> : file && url ? <Uploaded file={file} url={url} /> : <Form file={file} setFile={setFile} isPending={isPending} setIsPending={setIsPending} url={url} setUrl={setUrl} setError={setError}/>}
+        isPending ? <Pending/> : file && url ? <Uploaded summary={summary} /> : <UploadUsersProps file={file} setFile={setFile} isPending={isPending} setIsPending={setIsPending} url={url} setUrl={setUrl}  summary = {summary} setSummary = {setSummary} setError={setError}/>}
     </div>
   );
 }
