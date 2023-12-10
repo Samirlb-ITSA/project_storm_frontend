b73ea21abdfa6ed549fe3082d306fc9f89440776
juanevilla <juan.evilla@hotmail.com>
2023-12-09 20:03:21 -0500

    reports

--- src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import useAuthStore from "../stores/AuthStore"
-
-export const ProtectedRoute = ( {children, to}: any ) => {
-    const isAuthenticated = useAuthStore(state => state.session);
-
-    return isAuthenticated === null ? <Navigate to={to} /> : children;
-}
--- src/components/PublicRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import useAuthStore from "../stores/AuthStore"
-
-export const PublicRoutes = ( {children, to}: any ) => {
-    const isAuthenticated = useAuthStore(state => state.session);
-
-    return isAuthenticated === null ? children : <Navigate to={to} />;
-}
